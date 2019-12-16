ac987c3b3faaaa541e3de51b9c30422896fe44f7
itplamen@gmail.com <itplamen@gmail.com>
2019-12-16 23:12:45 +0200

    Created base data model and contracts

---
+++ Newsweek/Data/Newsweek.Models/Base/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Newsweek.Models.Base
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
