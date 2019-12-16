a99b12ffdd753b8462e30af4a33bb401301ef0af
itplamen@gmail.com <itplamen@gmail.com>
2019-12-16 23:25:38 +0200

    Created source and news data configurations

---
+++ Newsweek/Data/Newsweek.Data/Configurations/NewsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Newsweek.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Newsweek.Models;
+
+    public class NewsConfiguration : IEntityTypeConfiguration<News>
+    {
+        public void Configure(EntityTypeBuilder<News> news)
+        {
+            news
+                .HasOne(x => x.Source)
+                .WithMany(x => x.News)
+                .HasForeignKey(x => x.SourceId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Newsweek/Data/Newsweek.Data/Configurations/SourceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Newsweek.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    
+    using Newsweek.Models;
+
+    public class SourceConfiguration : IEntityTypeConfiguration<Source>
+    {
+        public void Configure(EntityTypeBuilder<Source> source)
+        {
+            source
+                .HasMany(x => x.News)
+                .WithOne(x => x.Source)
+                .HasForeignKey(x => x.SourceId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
