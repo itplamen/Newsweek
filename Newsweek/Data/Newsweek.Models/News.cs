200c754135d862e44351f072224121f193268845
itplamen@gmail.com <itplamen@gmail.com>
2019-12-16 23:17:45 +0200

    Created news and source data models

---
+++ Newsweek/Data/Newsweek.Models/News.cs
@@ -0,0 +1,25 @@
+﻿namespace Newsweek.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Newsweek.Models.Base;
+
+    public class News : BaseModel<int>
+    {
+        public string Author { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string RemoteUrl { get; set; }
+
+        public string RemoteUrlImage { get; set; }
+    }
+}
---
+++ Newsweek/Data/Newsweek.Models/Source.cs
@@ -0,0 +1,14 @@
+﻿namespace Newsweek.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Newsweek.Models.Base;
+
+    public class Source : BaseModel<int>
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string RemoteId { get; set; }
+    }
+}
