88ac0d7d7d8685cbb8b3d3d35da6f05b7ddacf23
itplamen@gmail.com <itplamen@gmail.com>
2019-12-16 23:26:50 +0200

    Created Newsweek db context

---
+++ Newsweek/Data/Newsweek.Data/NewsweekDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Newsweek.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    
+    using Newsweek.Models;
+
+    public class NewsweekDbContext : DbContext
+    {
+        public DbSet<Source> Sources { get; set; }
+
+        public DbSet<News> News { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) =>
+            builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+    }
+}
