fc0a1a2c1496804d5ca4dc744039ecc5ca3d6a4a
itplamen@gmail.com <itplamen@gmail.com>
2019-12-17 22:12:08 +0200

    Created Newsweek.Web MVC Core project

---
+++ Newsweek/Web/Newsweek.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Newsweek.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
