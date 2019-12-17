cc73697f763e262e990387a851a5939764a3d74b
itplamen@gmail.com <itplamen@gmail.com>
2019-12-17 21:49:00 +0200

    Created ApplicationRole data model

---
+++ Newsweek/Data/Newsweek.Models/ApplicationRole.cs
@@ -0,0 +1,30 @@
+﻿namespace Newsweek.Models
+{
+    using System;
+
+    using Microsoft.AspNetCore.Identity;
+
+    using Newsweek.Models.Base;
+
+    public class ApplicationRole : IdentityRole, IAuditInfo, IDeletableEntity
+    {
+        public ApplicationRole()
+               : this(null)
+        {
+        }
+
+        public ApplicationRole(string name)
+            : base(name)
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
