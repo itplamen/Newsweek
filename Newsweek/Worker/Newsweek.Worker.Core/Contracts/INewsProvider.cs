c933a2cff7abc41890c38f3d7b0254a476651535
itplamen@gmail.com <itplamen@gmail.com>
2019-12-23 23:50:12 +0200

    Created basic news provider

---
+++ Newsweek/Worker/Newsweek.Worker.Core/Contracts/INewsProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newsweek.Worker.Core.Contracts
+{
+    public interface INewsProvider
+    {
+        Task Get();
+    }
+}
---
+++ Newsweek/Worker/Newsweek.Worker.Core/Providers/EuronewsProvider.cs
@@ -0,0 +1,28 @@
+﻿using AngleSharp.Dom;
+using Newsweek.Worker.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newsweek.Worker.Core.Providers
+{
+    public class EuronewsProvider : INewsProvider
+    {
+        private const string URL = "https://www.euronews.com/news/europe";
+
+        private readonly INewsApi newsApi;
+
+        public EuronewsProvider(INewsApi newsApi)
+        {
+            this.newsApi = newsApi;
+        }
+
+        public async Task Get()
+        {
+            IDocument document = await newsApi.Get(URL);
+
+            IHtmlCollection<IElement> elements = document.QuerySelectorAll(".m-object--demi");
+        }
+    }
+}
