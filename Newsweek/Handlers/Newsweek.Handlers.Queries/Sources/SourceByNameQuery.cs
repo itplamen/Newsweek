6dbd4560334d89f325737996f41962901f32486d
itplamen@gmail.com <itplamen@gmail.com>
2019-12-24 23:42:21 +0200

    Created source by name query and handler

---
+++ Newsweek/Handlers/Newsweek.Handlers.Queries/Sources/SourceByNameQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace Newsweek.Handlers.Queries.Sources
+{
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Queries.Contracts;
+
+    public class SourceByNameQuery : IQuery<Source>
+    {
+        public SourceByNameQuery(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
