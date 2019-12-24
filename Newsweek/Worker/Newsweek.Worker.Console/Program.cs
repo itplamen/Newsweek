17500efd07d49af1e1fd4428316a90860f3774be
itplamen@gmail.com <itplamen@gmail.com>
2019-12-24 10:52:46 +0200

    Created base TasksExecutor

--- Newsweek/Worker/Newsweek.Worker.Console/Program.cs
+++ Newsweek/Worker/Newsweek.Worker.Console/Program.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using AngleSharp.Html.Parser;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newsweek.Worker.Core.Api;
+using Newsweek.Worker.Core.Contracts;
+using Newsweek.Worker.Core.Providers;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Newsweek.Worker.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            IHostBuilder hostBuilder = new HostBuilder()
+                .ConfigureServices(ConfigureServices);
+
+            await hostBuilder.RunConsoleAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<HttpClient>();
+            services.AddSingleton<IHtmlParser, HtmlParser>();
+
+            services.AddSingleton<INewsApi, NewsApi>();
+            services.AddTransient<INewsProvider, EuronewsProvider>();
+
+            services.AddHostedService<TasksExecutor>();
         }
     }
 }
---
+++ Newsweek/Worker/Newsweek.Worker.Console/TasksExecutor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Newsweek.Worker.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Newsweek.Worker.Console
+{
+    public class TasksExecutor : IHostedService
+    {
+        private readonly INewsProvider newsProvider;
+
+        public TasksExecutor(INewsProvider newsProvider)
+        {
+            this.newsProvider = newsProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await newsProvider.Get();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
