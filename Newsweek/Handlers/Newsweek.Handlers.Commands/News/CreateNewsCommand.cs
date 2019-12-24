63224b3704efcfc5470e60f0f17c33dd62f4bc7d
itplamen@gmail.com <itplamen@gmail.com>
2019-12-24 23:51:48 +0200

    Created command and handler for creating news entities

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/News/CreateNewsCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace Newsweek.Handlers.Commands.News
+{
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Handlers.Commands.Contracts;
+    using NewsData = Newsweek.Data.Models.News;
+
+    public class CreateNewsCommand : ICommand, IMapTo<NewsData>
+    {
+        public CreateNewsCommand(
+            string title, 
+            string description, 
+            string content, 
+            string remoteUrl, 
+            string mainImageUrl, 
+            int sourceId)
+        {
+            Title = title;
+            Description = description;
+            Content = content;
+            RemoteUrl = remoteUrl;
+            MainImageUrl = mainImageUrl;
+            SourceId = sourceId;
+        }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        public string RemoteUrl { get; set; }
+
+        public string MainImageUrl { get; set; }
+
+        public int SourceId { get; set; }
+    }
+}
---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/News/CreateNewsCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Newsweek.Handlers.Commands.News
+{
+    using System;
+
+    using AutoMapper;
+
+    using Newsweek.Data;
+    using Newsweek.Handlers.Commands.Contracts;
+    using NewsData = Newsweek.Data.Models.News;
+
+    public class CreateNewsCommandHandler : ICommandHandler<CreateNewsCommand>
+    {
+        private readonly NewsweekDbContext dbContext;
+
+        public CreateNewsCommandHandler(NewsweekDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Handle(CreateNewsCommand command)
+        {
+            NewsData news = Mapper.Map<NewsData>(command);
+            news.CreatedOn = DateTime.UtcNow;
+
+            dbContext.Add(news);
+            dbContext.SaveChanges();
+        }
+    }
+}
