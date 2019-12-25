4986f8433bb96e0f10aee17480b07a95f2853bad
itplamen@gmail.com <itplamen@gmail.com>
2019-12-25 23:01:20 +0200

    Moved TasksExecutor.cs from Newsweek.Worker.Console into Newsweek.Worker.Core project

---
+++ Newsweek/Worker/Newsweek.Worker.Core/Tasks/TasksExecutor.cs
@@ -0,0 +1,30 @@
+﻿namespace Newsweek.Worker.Core.Tasks
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Hosting;
+    
+    using Newsweek.Worker.Core.Contracts;
+
+    public class TasksExecutor : IHostedService
+    {
+        private readonly ITask newsTask;
+
+        public TasksExecutor(ITask newsTask)
+        {
+            this.newsTask = newsTask;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await newsTask.DoWork();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
