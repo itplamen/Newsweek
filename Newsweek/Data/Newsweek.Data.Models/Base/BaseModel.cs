76a0a8eae27039eae0495232432027e3127b6028
itplamen@gmail.com <itplamen@gmail.com>
2019-12-30 01:04:59 +0200

    Added IDeletableEntity contract to BaseModel

--- Newsweek/Data/Newsweek.Data.Models/Base/BaseModel.cs
+++ Newsweek/Data/Newsweek.Data.Models/Base/BaseModel.cs
@@ -3,7 +3,7 @@
     using System;
     using System.ComponentModel.DataAnnotations;
 
-    public abstract class BaseModel<TKey> : IAuditInfo
+    public abstract class BaseModel<TKey> : IAuditInfo, IDeletableEntity
     {
         [Key]
         public TKey Id { get; set; }
