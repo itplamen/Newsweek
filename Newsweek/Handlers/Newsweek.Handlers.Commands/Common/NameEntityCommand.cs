9f5a2b3e5befd62e6607e8b873a26fc28397a41e
itplamen@gmail.com <itplamen@gmail.com>
2020-01-13 23:33:25 +0200

    Created common name entity command

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Common/NameEntityCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Newsweek.Handlers.Commands.Common
+{
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Commands.Contracts;
+
+    public class NameEntityCommand<TEntity, TKey> : ICommand, IMapTo<TEntity>
+        where TEntity : BaseModel<TKey>
+    {
+        public NameEntityCommand(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
