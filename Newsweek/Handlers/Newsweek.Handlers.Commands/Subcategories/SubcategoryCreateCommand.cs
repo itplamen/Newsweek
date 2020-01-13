5dd8fe5652b3d8f8f8ae5f5a02f7459366c8f65a
itplamen@gmail.com <itplamen@gmail.com>
2020-01-14 00:01:01 +0200

    Added command for creating subcategories

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Subcategories/SubcategoryCreateCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Newsweek.Handlers.Commands.Subcategories
+{
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Commands.Contracts;
+
+    public class SubcategoryCreateCommand : ICommand, IMapTo<Subcategory>
+    {
+        public SubcategoryCreateCommand(string name, int categoryId)
+        {
+            Name = name;
+            CategoryId = categoryId;
+        }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
