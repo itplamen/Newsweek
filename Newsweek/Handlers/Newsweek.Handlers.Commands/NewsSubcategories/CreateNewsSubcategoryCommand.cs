8215018178c25f2144bc4c151d31e5cdd85eea11
itplamen@gmail.com <itplamen@gmail.com>
2020-01-18 20:04:42 +0200

    Added command for creating NewsSubcategory entities

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/NewsSubcategories/CreateNewsSubcategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Newsweek.Handlers.Commands.NewsSubcategories
+{
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Commands.Contracts;
+
+    public class CreateNewsSubcategoryCommand : ICommand, IMapTo<NewsSubcategory>
+    {
+        public int NewsId { get; set; }
+
+        public int SubcategoryId { get; set; }
+    }
+}
