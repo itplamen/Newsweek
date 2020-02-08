7bd6dda03ddbd7f6a977819f8a1b34a1198d2775
itplamen@gmail.com <itplamen@gmail.com>
2020-02-08 11:41:10 +0200

    Created query and handler for selecting entities

---
+++ Newsweek/Handlers/Newsweek.Handlers.Queries/Common/SelectEntitiesQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace Newsweek.Handlers.Queries.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Queries.Contracts;
+    
+    public class SelectEntitiesQuery<TEntity, TResult> : IQuery<IEnumerable<TResult>>
+        where TEntity : BaseModel<int>
+    {
+        public Expression<Func<TEntity, bool>> Predicate { get; set; }
+
+        public Expression<Func<TEntity, TResult>> Selector { get; set; }
+    }
+}
---
+++ Newsweek/Handlers/Newsweek.Handlers.Queries/Common/SelectEntitiesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Newsweek.Handlers.Queries.Common
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    
+    using Newsweek.Data;
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Queries.Contracts;
+    
+    public class SelectEntitiesQueryHandler<TEntity, TResult> : IQueryHandler<SelectEntitiesQuery<TEntity, TResult>, IEnumerable<TResult>>
+        where TEntity : BaseModel<int>
+    {
+        private readonly NewsweekDbContext dbContext;
+
+        public SelectEntitiesQueryHandler(NewsweekDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<TResult>> Handle(SelectEntitiesQuery<TEntity, TResult> query)
+        {
+            IQueryable<TEntity> entities = dbContext.Set<TEntity>();
+
+            if (query.Predicate != null)
+            {
+                entities = entities.Where(query.Predicate);
+            }
+
+            return await entities.Select(query.Selector).ToListAsync();
+        }
+    }
+}
