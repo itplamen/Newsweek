7f5e2a1c5928f1f08d2376a5e8b72870b86a8ea5
itplamen@gmail.com <itplamen@gmail.com>
2020-02-27 00:42:24 +0200

    #4 Created command and handler for sending emails

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Emails/SendEmailCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Newsweek.Handlers.Commands.Emails
+{
+    using System.Collections.Generic;
+
+    using MediatR;
+
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Data.Models;
+
+    public class SendEmailCommand : IRequest, IMapTo<Email>
+    {
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+
+        public IEnumerable<string> Emails { get; set; }
+    }
+}
---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Emails/SendEmailCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿namespace Newsweek.Handlers.Commands.Emails
+{
+	using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+	using System.Threading.Tasks;
+
+	using MediatR;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    
+    using SendGrid;
+	using SendGrid.Helpers.Mail;
+
+	public class SendEmailCommandHandler : IRequestHandler<SendEmailCommand>
+    {
+		private readonly ISendGridClient client;
+        private readonly ILogger<SendEmailCommandHandler> logger;
+        private readonly IConfiguration configuration;
+
+        public SendEmailCommandHandler(ISendGridClient client, ILogger<SendEmailCommandHandler> logger, IConfiguration configuration)
+		{
+			this.client = client;
+            this.logger = logger;
+            this.configuration = configuration;
+		}
+
+		public async Task<Unit> Handle(SendEmailCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                EmailAddress from = new EmailAddress(configuration["SendGrid:FromEmail"]);
+                List<EmailAddress> to = request.Emails
+                    .Select(x => new EmailAddress(x))
+                    .ToList();
+
+                SendGridMessage message = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, request.Subject, null, request.Content);
+
+                Response response = await client.SendEmailAsync(message, cancellationToken);
+                logger.LogInformation("Sent email successfully", request, response);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not send email", request);
+            }
+
+            return Unit.Value;
+		}
+    }
+}
