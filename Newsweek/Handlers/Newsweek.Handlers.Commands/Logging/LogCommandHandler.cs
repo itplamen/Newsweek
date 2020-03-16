7481b0f403ff06abbea967d87ecc98981bf94d69
itplamen@gmail.com <itplamen@gmail.com>
2020-03-16 23:29:01 +0200

    #2 Created log command and handler

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Logging/LogCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace Newsweek.Handlers.Commands.Logging
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using MediatR;
+
+    using Newsweek.Common.Infrastructure.Logging;
+    using Newsweek.Data.Models;
+    using Newsweek.Handlers.Commands.Common;
+
+    public class LogCommandHandler : IRequestHandler<LogCommand>
+    {
+        private readonly IMediator mediator;
+        private readonly ILoggerStorage loggerStorage;
+
+        public LogCommandHandler(IMediator mediator, ILoggerStorage loggerStorage)
+        {
+            this.mediator = mediator;
+            this.loggerStorage = loggerStorage;
+        }
+
+        public async Task<Unit> Handle(LogCommand request, CancellationToken cancellationToken)
+        {
+            ICollection<LogCommand> logCommands = new List<LogCommand>();
+
+            while (loggerStorage.HasAny())
+            {
+                LogData logData = loggerStorage.Get();
+                LogCommand logCommand =  Mapper.Map<LogCommand>(logData);
+
+                logCommands.Add(logCommand);
+            }
+
+            await mediator.Send(new CreateEntitiesCommand<Log>(logCommands), cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
