697394c15002cba23cfa1044023fa2d225657d68
itplamen@gmail.com <itplamen@gmail.com>
2020-03-17 23:44:37 +0200

    Added forecast iframe, resolves #6

--- Newsweek/Web/Newsweek.Web/Controllers/HomeController.cs
+++ Newsweek/Web/Newsweek.Web/Controllers/HomeController.cs
@@ -32,11 +32,6 @@
             return View(topNews);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
