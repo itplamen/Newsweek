c5046260a50cc149e9aa1bb3029b4b22ea562b0c
itplamen@gmail.com <itplamen@gmail.com>
2020-03-19 22:02:28 +0200

    Added scaffolded views and page models

---
+++ Newsweek/Web/Newsweek.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Newsweek.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Newsweek.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
