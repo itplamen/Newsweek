723f71b704d3ad8b2ceef1d1ec85acc9a8dcea4a
itplamen@gmail.com <itplamen@gmail.com>
2020-03-24 21:21:31 +0200

    #10 Created common handler for deleting entities

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Common/Delete/DeleteEntityCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Newsweek.Handlers.Commands.Common.Delete
+{
+    using MediatR;
+    
+    using Newsweek.Data.Models;
+
+    public class DeleteEntityCommand<TEntity> : IRequest<bool>
+        where TEntity : BaseModel<int>
+    {
+        public DeleteEntityCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Common/Delete/DeleteEntityCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace Newsweek.Handlers.Commands.Common.Delete
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MediatR;
+
+    using Newsweek.Data;
+    using Newsweek.Data.Models;
+
+    public class DeleteEntityCommandHandler<TEntity> : IRequestHandler<DeleteEntityCommand<TEntity>, bool>
+        where TEntity : BaseModel<int>
+    {
+        private readonly NewsweekDbContext dbContext;
+
+        public DeleteEntityCommandHandler(NewsweekDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteEntityCommand<TEntity> request, CancellationToken cancellationToken)
+        {
+            TEntity entity = dbContext.Set<TEntity>()
+                .FirstOrDefault(x => x.Id == request.Id);
+
+            if (entity != null)
+            {
+                entity.IsDeleted = true;
+                entity.DeletedOn = DateTime.UtcNow;
+
+                await dbContext.SaveChangesAsync(cancellationToken);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
