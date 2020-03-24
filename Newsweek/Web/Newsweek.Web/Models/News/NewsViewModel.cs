512e3f50cc16d0687214cd9a4d8496ed54e6ac82
itplamen@gmail.com <itplamen@gmail.com>
2020-03-25 00:22:00 +0200

    #10 Show only active comments

--- Newsweek/Web/Newsweek.Web/Models/News/NewsViewModel.cs
+++ Newsweek/Web/Newsweek.Web/Models/News/NewsViewModel.cs
@@ -1,14 +1,17 @@
 ﻿namespace Newsweek.Web.Models.News
 {
     using System.Collections.Generic;
+    using System.Linq;
 
+    using AutoMapper;
+    
     using Newsweek.Common.Infrastructure.Mapping;
     using Newsweek.Web.Models.Comments;
     using Newsweek.Web.Models.Common;
     using Newsweek.Web.Models.Sources;
     using Newsweek.Web.Models.Subcategories;
 
-    public class NewsViewModel : BaseViewModel, IMapFrom<Data.Models.News>
+    public class NewsViewModel : BaseViewModel, IMapFrom<Data.Models.News>, IHaveCustomMappings
     {
         public string Title { get; set; }
 
@@ -25,5 +28,11 @@
         public SubcategoryViewModel Subcategory { get; set; }
 
         public IEnumerable<GetCommentViewModel> Comments { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Data.Models.News, NewsViewModel>()
+                .ForMember(x => x.Comments, opt => opt.MapFrom(x => x.Comments.Where(y => !y.IsDeleted)));
+        }
     }
 }
