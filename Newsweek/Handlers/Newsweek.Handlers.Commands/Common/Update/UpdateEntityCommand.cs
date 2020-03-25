29ed9547c407e75d8f0da8a82099cf9022df38ec
itplamen@gmail.com <itplamen@gmail.com>
2020-03-26 00:07:24 +0200

    #10 Created common handler for updating comments

---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Common/Update/UpdateEntityCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Newsweek.Handlers.Commands.Common.Update
+{
+    using MediatR;
+
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Data.Models;
+    
+    public class UpdateEntityCommand<TEntity, TSource> : IRequest<bool>
+        where TEntity : BaseModel<int>
+        where TSource : IMapTo<TEntity>
+    {
+        public UpdateEntityCommand(TSource source)
+        {
+            Source = source;
+        }
+
+        public TSource Source { get; set; }
+    }
+}
---
+++ Newsweek/Handlers/Newsweek.Handlers.Commands/Common/Update/UpdateEntityCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿namespace Newsweek.Handlers.Commands.Common.Update
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+    
+    using MediatR;
+
+    using Newsweek.Common.Infrastructure.Mapping;
+    using Newsweek.Data;
+    using Newsweek.Data.Models;
+    
+    public class UpdateEntityCommandHandler<TEntity, TSource> : IRequestHandler<UpdateEntityCommand<TEntity, TSource>, bool>
+        where TEntity : BaseModel<int>
+        where TSource : IMapTo<TEntity>
+    {
+        private readonly NewsweekDbContext dbContext;
+
+        public UpdateEntityCommandHandler(NewsweekDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(UpdateEntityCommand<TEntity, TSource> request, CancellationToken cancellationToken)
+        {
+            TEntity entity = Mapper.Map<TEntity>(request.Source);
+            TEntity entityToUpdate = dbContext.Set<TEntity>().FirstOrDefault(x => x.Id == entity.Id);
+
+            if (entityToUpdate != null)
+            {
+                var propertiesToUpdate = request.Source.GetType().GetProperties()
+                    .Where(x => x.Name != nameof(entity.Id));
+
+                foreach (var property in propertiesToUpdate)
+                {
+                    var valueToUpdate = property.GetValue(request.Source);
+                    entityToUpdate.GetType().GetProperty(property.Name).SetValue(entityToUpdate, valueToUpdate);
+                }
+
+                entityToUpdate.ModifiedOn = DateTime.UtcNow;
+
+                await dbContext.SaveChangesAsync(cancellationToken);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
