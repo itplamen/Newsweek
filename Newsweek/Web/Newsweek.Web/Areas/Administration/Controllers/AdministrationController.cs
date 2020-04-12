95cb9f99c53876158fd1cb8a0e6ce2c33b8153f7
itplamen@gmail.com <itplamen@gmail.com>
2020-04-13 01:04:45 +0300

    #9 Created base administration controller

---
+++ Newsweek/Web/Newsweek.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,10 @@
+﻿namespace Newsweek.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Administration")]
+    //[Authorize(Roles = "Administrator")]
+    public abstract class AdministrationController : Controller
+    {
+    }
+}
