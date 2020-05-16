81b4880772e2b771cbd26be439a0f8b46873672c
itplamen@gmail.com <itplamen@gmail.com>
2020-05-17 01:09:36 +0300

    #14 Added some tests

---
+++ Newsweek/Tests/Common/Newsweek.Tests.Common/Mocks/NewsApiMock.cs
@@ -0,0 +1,23 @@
+﻿namespace Newsweek.Tests.Common.Mocks
+{
+    using System.Threading.Tasks;
+
+    using AngleSharp.Dom;
+
+    using Moq;
+
+    using Newsweek.Worker.Core.Contracts;
+    
+    public static class NewsApiMock
+    {
+        public static INewsApi GetNewsApi()
+        {
+            var newsApi = new Mock<INewsApi>();
+
+            newsApi.Setup(x => x.Get(It.IsAny<string>()))
+                .Returns(Task.FromResult<IDocument>(null));
+
+            return newsApi.Object;
+        }
+    }
+}
---
+++ Newsweek/Tests/Worker/Newsweek.Worker.Core.Tests/Providers/EuropeNewsProviderTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Newsweek.Worker.Core.Tests.Providers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Xunit;
+
+    using Newsweek.Data.Models;
+    using Newsweek.Tests.Common.Mocks;
+    using Newsweek.Worker.Core.Contracts;
+    using Newsweek.Worker.Core.Providers;
+
+    public class EuropeNewsProviderTests
+    {
+        private readonly Source source;
+        private readonly Category category;
+        private readonly INewsProvider newsProvider;
+
+        public EuropeNewsProviderTests()
+        {
+            this.source = new Source()
+            {
+                Name = "Euronews",
+                Url = "https://www.euronews.com"
+            };
+
+            this.category = new Category()
+            {
+                Name = "Europe"
+            };
+
+            this.newsProvider = new EuropeNewsProvider(NewsApiMock.GetNewsApi());
+        }
+
+        [Fact]
+        public async Task EuropeNewsProviderWithMissingSourceShouldThrowArgumentException()
+        {
+            await Assert.ThrowsAsync<ArgumentException>(() => newsProvider.Get(null, category));
+        }
+
+        [Fact]
+        public async Task EuropeNewsProviderWithMissingCategoryShouldThrowArgumentException()
+        {
+            await Assert.ThrowsAsync<ArgumentException>(() => newsProvider.Get(source, null));
+        }
+    }
+}
